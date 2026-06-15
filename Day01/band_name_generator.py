208a51c453171c38b1140b2ecaa472924967a27c
Ashwini Singh <ashwinikr.singh97@gmail.com>
2026-06-15 17:02:00 +0530

    Update band_name_generator.py

--- Day01/band_name_generator.py
+++ Day01/band_name_generator.py
@@ -2,8 +2,8 @@
 print("Welcome to the Band Name Generator")
 
 #City and Pet names stored in variables
-city = input("What's the name of the city you grew in?")
-pet = input("What's your pet's name?")
+city = input("What's the name of the city you grew in?\n")
+pet = input("What's your pet's name?\n")
 
 #Combining the name for band name generation
 print("Your band name could be " + city + " " + pet)
